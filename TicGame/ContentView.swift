cae2cc3d1cf8d16487cca23771c39f63f760fd2c
jordsek <73316853+jordsek@users.noreply.github.com>
2023-12-06 15:44:40 +0000

    first commit

---
+++ TicGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicGame
+//
+//  Created by Jordan Isac on 06/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicGame/TicGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicGameApp.swift
+//  TicGame
+//
+//  Created by Jordan Isac on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TicGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
